e7ca6276372fc38f4821182183a5663fe7352aeb
iltonkp <iltonk.si@gmail.com>
2024-08-20 21:45:26 +0100

    feat: initial structure

---
+++ udemy-hexagonal-architecture/src/main/java/io/github/iltonkp/udemy_hexagonal_architecture/UdemyHexagonalArchitectureApplication.java
@@ -0,0 +1,13 @@
+package io.github.iltonkp.udemy_hexagonal_architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyHexagonalArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyHexagonalArchitectureApplication.class, args);
+	}
+
+}
---
+++ udemy-hexagonal-architecture/src/test/java/io/github/iltonkp/udemy_hexagonal_architecture/UdemyHexagonalArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.iltonkp.udemy_hexagonal_architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyHexagonalArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
