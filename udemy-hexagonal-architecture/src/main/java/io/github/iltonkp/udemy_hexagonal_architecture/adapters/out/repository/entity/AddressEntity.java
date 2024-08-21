9463997e3e4248d406306d1c0557093025dfc62e
iltonkp <iltonk.si@gmail.com>
2024-08-21 19:01:14 +0100

    feat(8): creating repository

---
+++ udemy-hexagonal-architecture/src/main/java/io/github/iltonkp/udemy_hexagonal_architecture/adapters/out/repository/entity/AddressEntity.java
@@ -0,0 +1,11 @@
+package io.github.iltonkp.udemy_hexagonal_architecture.adapters.out.repository.entity;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+public class AddressEntity {
+    private String street;
+    private String city;
+    private String state;
+}
