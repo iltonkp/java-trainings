86e433e5a3bf8645c354a88d0fc78981f4f48d44
iltonkp <iltonk.si@gmail.com>
2024-08-21 18:42:41 +0100

    feat(7): creating search address adapter

---
+++ udemy-hexagonal-architecture/src/main/java/io/github/iltonkp/udemy_hexagonal_architecture/adapters/out/FindAddressByZipCodeAdapter.java
@@ -0,0 +1,23 @@
+package io.github.iltonkp.udemy_hexagonal_architecture.adapters.out;
+
+import io.github.iltonkp.udemy_hexagonal_architecture.adapters.out.client.FindAddressByZipCodeClient;
+import io.github.iltonkp.udemy_hexagonal_architecture.adapters.out.client.mapper.AddressResponseMapper;
+import io.github.iltonkp.udemy_hexagonal_architecture.application.core.domain.Address;
+import io.github.iltonkp.udemy_hexagonal_architecture.application.ports.out.FindAddressByZipCodOutputPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class FindAddressByZipCodeAdapter implements FindAddressByZipCodOutputPort {
+
+    private  FindAddressByZipCodeClient findAddressByZipCodeClient;
+    private  AddressResponseMapper addressResponseMapper;
+
+
+    @Override
+    public Address find(String zipCode) {
+        var addressResponse = findAddressByZipCodeClient.find(zipCode);
+        return addressResponseMapper.toAddress(addressResponse);
+    }
+}
