7478f6668efd5501e7dca0aeaf0ad10cd8abeb30
iltonkp <iltonk.si@gmail.com>
2024-08-25 10:57:29 +0100

    feat(12): creating find customer by id adapter and mapper

---
+++ udemy-hexagonal-architecture/src/main/java/io/github/iltonkp/udemy_hexagonal_architecture/adapters/out/FindCustomerByIdAdapter.java
@@ -0,0 +1,25 @@
+package io.github.iltonkp.udemy_hexagonal_architecture.adapters.out;
+
+import io.github.iltonkp.udemy_hexagonal_architecture.adapters.out.repository.CustomerRepository;
+import io.github.iltonkp.udemy_hexagonal_architecture.adapters.out.repository.mapper.CustomerEntityMapper;
+import io.github.iltonkp.udemy_hexagonal_architecture.application.core.domain.Customer;
+import io.github.iltonkp.udemy_hexagonal_architecture.application.ports.out.FindCustomerByIdOutputPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Component
+public class FindCustomerByIdAdapter implements FindCustomerByIdOutputPort {
+
+    private CustomerRepository customerRepository;
+    private CustomerEntityMapper customerEntityMapper;
+
+
+    @Override
+    public Optional<Customer> find(String id) {
+       var customerEntity = customerRepository.findById(id);
+       return customerEntity.map(entity -> customerEntityMapper.toCustomer(entity));
+    }
+}
