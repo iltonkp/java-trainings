9e3ea45979dcfdfb003769c4de83c9fdd009106a
iltonkp <iltonk.si@gmail.com>
2024-08-25 12:10:55 +0100

    feat(18): creating delete customer by id adapter

---
+++ udemy-hexagonal-architecture/src/main/java/io/github/iltonkp/udemy_hexagonal_architecture/adapters/out/DeleteCustomerByIdAdapter.java
@@ -0,0 +1,22 @@
+package io.github.iltonkp.udemy_hexagonal_architecture.adapters.out;
+
+import io.github.iltonkp.udemy_hexagonal_architecture.adapters.out.repository.CustomerRepository;
+import io.github.iltonkp.udemy_hexagonal_architecture.application.ports.out.DeleteCustomerByIdOutputPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DeleteCustomerByIdAdapter implements DeleteCustomerByIdOutputPort {
+
+    private CustomerRepository customerRepository;
+
+
+    @Override
+    public void delete(String id) {
+
+        customerRepository.deleteById(id);
+
+    }
+
+}
