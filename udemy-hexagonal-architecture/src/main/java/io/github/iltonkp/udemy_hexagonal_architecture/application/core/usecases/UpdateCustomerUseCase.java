52b2f42b17f78906d15c716532c9c6a2fc802d1d
iltonkp <iltonk.si@gmail.com>
2024-08-25 11:32:27 +0100

    feat(14): creating update customer use case and output port

---
+++ udemy-hexagonal-architecture/src/main/java/io/github/iltonkp/udemy_hexagonal_architecture/application/core/usecases/UpdateCustomerUseCase.java
@@ -0,0 +1,30 @@
+package io.github.iltonkp.udemy_hexagonal_architecture.application.core.usecases;
+
+import io.github.iltonkp.udemy_hexagonal_architecture.application.core.domain.Customer;
+import io.github.iltonkp.udemy_hexagonal_architecture.application.ports.in.FindCustomerByIdInputPort;
+import io.github.iltonkp.udemy_hexagonal_architecture.application.ports.out.FindAddressByZipCodOutputPort;
+import io.github.iltonkp.udemy_hexagonal_architecture.application.ports.out.UpdateCustomerOutputPort;
+
+public class UpdateCustomerUseCase {
+
+    private final FindCustomerByIdInputPort findCustomerByIdInputPort;
+    private final FindAddressByZipCodOutputPort findAddressByZipCodOutputPort;
+    private final UpdateCustomerOutputPort updateCustomerOutputPort;
+
+    public UpdateCustomerUseCase(
+            FindCustomerByIdInputPort findCustomerByIdInputPort,
+            FindAddressByZipCodOutputPort findAddressByZipCodOutputPort,
+            UpdateCustomerOutputPort updateCustomerOutputPort
+    ) {
+        this.findCustomerByIdInputPort = findCustomerByIdInputPort;
+        this.findAddressByZipCodOutputPort = findAddressByZipCodOutputPort;
+        this.updateCustomerOutputPort = updateCustomerOutputPort;
+    }
+
+    public void update(Customer customer, String zipCode){
+        findCustomerByIdInputPort.find(customer.getId());
+        var address = findAddressByZipCodOutputPort.find(zipCode);
+        customer.setAddress(address);
+        updateCustomerOutputPort.update(customer);
+    }
+}
