448c631478e2187239bc8a441d89fd6db1e2a177
iltonkp <iltonk.si@gmail.com>
2024-08-25 11:37:19 +0100

    feat(15): creating update customer adapter

---
+++ udemy-hexagonal-architecture/src/main/java/io/github/iltonkp/udemy_hexagonal_architecture/adapters/out/UpdateCustomerAdapter.java
@@ -0,0 +1,24 @@
+package io.github.iltonkp.udemy_hexagonal_architecture.adapters.out;
+
+import io.github.iltonkp.udemy_hexagonal_architecture.adapters.out.repository.CustomerRepository;
+import io.github.iltonkp.udemy_hexagonal_architecture.adapters.out.repository.mapper.CustomerEntityMapper;
+import io.github.iltonkp.udemy_hexagonal_architecture.application.core.domain.Customer;
+import io.github.iltonkp.udemy_hexagonal_architecture.application.ports.out.UpdateCustomerOutputPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UpdateCustomerAdapter implements UpdateCustomerOutputPort {
+
+    private CustomerRepository customerRepository;
+    private CustomerEntityMapper customerEntityMapper;
+
+
+    @Override
+    public void update(Customer customer) {
+        var customerEntity = customerEntityMapper.toCustomerEntity(customer);
+        customerRepository.save(customerEntity);
+    }
+
+}
