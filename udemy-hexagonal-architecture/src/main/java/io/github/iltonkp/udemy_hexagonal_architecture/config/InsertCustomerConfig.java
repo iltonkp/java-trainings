965f6f5ad6792b3ed679888896a36afc7a8a02b6
iltonkp <iltonk.si@gmail.com>
2024-08-25 15:35:15 +0100

    feat(22): creating kafka producer

--- udemy-hexagonal-architecture/src/main/java/io/github/iltonkp/udemy_hexagonal_architecture/config/InsertCustomerConfig.java
+++ udemy-hexagonal-architecture/src/main/java/io/github/iltonkp/udemy_hexagonal_architecture/config/InsertCustomerConfig.java
@@ -2,6 +2,7 @@ package io.github.iltonkp.udemy_hexagonal_architecture.config;
 
 import io.github.iltonkp.udemy_hexagonal_architecture.adapters.out.FindAddressByZipCodeAdapter;
 import io.github.iltonkp.udemy_hexagonal_architecture.adapters.out.InsertCustomerAdapter;
+import io.github.iltonkp.udemy_hexagonal_architecture.adapters.out.SendCpfForValidationAdapter;
 import io.github.iltonkp.udemy_hexagonal_architecture.application.core.usecases.InsertCustomerUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +13,11 @@ public class InsertCustomerConfig {
     @Bean
     public InsertCustomerUseCase insertCustomerUseCase(
             FindAddressByZipCodeAdapter findAddressByZipCodeAdapter,
-            InsertCustomerAdapter insertCustomerAdapter
+            InsertCustomerAdapter insertCustomerAdapter,
+            SendCpfForValidationAdapter sendCpfForValidationAdapter
     ) {
 
-        return new InsertCustomerUseCase(findAddressByZipCodeAdapter, insertCustomerAdapter);
+        return new InsertCustomerUseCase(findAddressByZipCodeAdapter, insertCustomerAdapter, sendCpfForValidationAdapter);
 
     }
 
