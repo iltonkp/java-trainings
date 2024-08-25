1ec93b5d15fbb2f8609971603a564ba3c50aa548
iltonkp <iltonk.si@gmail.com>
2024-08-25 15:25:31 +0100

    feat(21): creating kafka configuration

--- udemy-hexagonal-architecture/src/main/java/io/github/iltonkp/udemy_hexagonal_architecture/adapters/in/consumer/message/CustomerMessage.java
+++ udemy-hexagonal-architecture/src/main/java/io/github/iltonkp/udemy_hexagonal_architecture/adapters/in/consumer/message/CustomerMessage.java
@@ -1,6 +1,5 @@
 package io.github.iltonkp.udemy_hexagonal_architecture.adapters.in.consumer.message;
 
-import io.github.iltonkp.udemy_hexagonal_architecture.application.core.domain.Address;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
