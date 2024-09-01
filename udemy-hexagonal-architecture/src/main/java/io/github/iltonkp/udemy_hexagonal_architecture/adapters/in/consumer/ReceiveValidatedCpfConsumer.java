23e3674a410746ae21145503359c431828a1b9a4
iltonkp <iltonk.si@gmail.com>
2024-09-01 15:28:00 +0100

    feat(23): creating kafka consumer

---
+++ udemy-hexagonal-architecture/src/main/java/io/github/iltonkp/udemy_hexagonal_architecture/adapters/in/consumer/ReceiveValidatedCpfConsumer.java
@@ -0,0 +1,25 @@
+package io.github.iltonkp.udemy_hexagonal_architecture.adapters.in.consumer;
+
+import io.github.iltonkp.udemy_hexagonal_architecture.adapters.in.consumer.mapper.CustomerMessageMapper;
+import io.github.iltonkp.udemy_hexagonal_architecture.adapters.in.consumer.message.CustomerMessage;
+import io.github.iltonkp.udemy_hexagonal_architecture.application.ports.in.UpdateCustomerInputPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ReceiveValidatedCpfConsumer {
+
+    private UpdateCustomerInputPort updateCustomerInputPort;
+    private CustomerMessageMapper customerMessageMapper;
+
+    @KafkaListener(topics = "tp-cpf-validated", groupId = "iltonkp")
+    public void receive( CustomerMessage customerMessage) {
+
+        var customer = customerMessageMapper.toCustomer(customerMessage);
+        updateCustomerInputPort.update(customer, customerMessage.getZipCode());
+
+    }
+
+}
--- udemy-hexagonal-architecture/src/main/java/io/github/iltonkp/udemy_hexagonal_architecture/adapters/in/consumer/message/CustomerMessage.java
+++ udemy-hexagonal-architecture/src/main/java/io/github/iltonkp/udemy_hexagonal_architecture/adapters/in/consumer/message/CustomerMessage.java
@@ -4,9 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class CustomerMessage {
 
     private String id;
